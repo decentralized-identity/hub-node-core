d05be67133c3da516496506c128ae00d907d4b82
Logan Girvin <logirvin@microsoft.com>
2018-09-18 18:17:33 +0000

    Merged PR 11: Added Code Coverage

--- tests/helpers/reporter.js
+++ tests/helpers/reporter.js
@@ -1,9 +1,9 @@
 // this is a Jasmine helper function used to export results as xunit tests results.
 var jasmineReporters = require('jasmine-reporters');
 
-var junitReporter = new jasmineReporters.NUnitXmlReporter({
+var nunitReporter = new jasmineReporters.NUnitXmlReporter({
   savePath: './',
   consolidateAll: false,
 });
 
-jasmine.getEnv().addReporter(junitReporter)
+jasmine.getEnv().addReporter(nunitReporter)
