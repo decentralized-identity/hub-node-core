4ad81c9fb01dd43e64892007719a8e7d41cb0cde
Logan Girvin <logirvin@microsoft.com>
2018-11-16 14:39:46 -0800

    Request is now BaseRequest, since commits contain interfaces.

--- lib/models/BaseRequest.ts
+++ lib/models/BaseRequest.ts
@@ -1,23 +1,54 @@
-import Request from './Request';
+import HubError, { ErrorCode, DeveloperMessage } from './HubError';
 
 /**
- * A hub request of type BaseRequest
+ * A generic hub request. All requests contain these fields
  */
-export default class BaseRequest extends Request {
-  /** interface of the request */
-  readonly interface: string;
+export default class BaseRequest {
+  /** \@context of the request schema */
+  protected readonly context = 'https://schema.identity.foundation/0.1';
+  /** \@type of the request, always 'BaseRequest' */
+  protected type: string;
+  /** did of the issuer of the request */
+  readonly iss: string;
+  /** did of the intended recipient of the request */
+  readonly aud: string;
+  /** did of the indended audiance of the request */
+  readonly sub: string;
 
   /**
    * Constructs a request from the request JSON
    * @param json A json string or object
    */
   constructor(json: string | any) {
-    super(json);
     let request = json;
     if (typeof json === 'string') {
       request = JSON.parse(json);
     }
-    this.interface = request.interface;
+    if (this.context !== request['@context']) {
+      throw new HubError({
+        errorCode: ErrorCode.BadRequest,
+        property: '@context',
+        developerMessage: DeveloperMessage.IncorrectParameter,
+      });
+    }
+    if (!('@type' in request)) {
+      throw new HubError({
+        errorCode: ErrorCode.BadRequest,
+        property: '@type',
+        developerMessage: DeveloperMessage.MissingParameter,
+      });
+    }
+    if (typeof request['@type'] !== 'string') {
+      throw new HubError({
+        errorCode: ErrorCode.BadRequest,
+        property: '@type',
+        developerMessage: DeveloperMessage.IncorrectParameter,
+      });
+    }
+    this.type = request['@type'];
+    this.iss = request.iss;
+    this.aud = request.aud;
+    this.sub = request.sub;
   }
 
   /**
