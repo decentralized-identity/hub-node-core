3ad9572009ffbbebc33984d80f8189ac74500630
Logan Girvin <logirvin@microsoft.com>
2018-11-27 13:49:44 -0800

    added other cases for strings utility.

--- tests/Utilities.spec.ts
+++ tests/Utilities.spec.ts
@@ -5,6 +5,11 @@ describe('Utilities', () => {
       it ('should make the first letter of test uppercase', () => {
         const test = 'test';
         expect(Strings.upperFirst(test)).toBe('Test');
+        const singleLetter = 'a';
+        expect(Strings.upperFirst(singleLetter)).toBe('A');
+        const noOp = '';
+        expect(Strings.upperFirst(noOp)).toBe('');
       });
+      
   });
 });
