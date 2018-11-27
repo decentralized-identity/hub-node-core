a4e65c12259b3e2d74e10a696aea8735dc8bf41f
Logan Girvin <logirvin@microsoft.com>
2018-11-26 16:02:49 -0800

    added final tests for HubError

--- tests/models/HubError.spec.ts
+++ tests/models/HubError.spec.ts
@@ -1,4 +1,4 @@
-import HubError, { ErrorCode } from '../../lib/models/HubError';
+import HubError, { ErrorCode, DeveloperMessage } from '../../lib/models/HubError';
 
 describe('HubError', () => {
   describe('constructor', () => {
@@ -25,5 +25,14 @@ describe('HubError', () => {
       expect(response.errorCode).toEqual(error.errorCode);
       expect(response.developerMessage).toEqual(error.developerMessage);
     });
+
+    it('should auto fill the developer message for not implemented', () => {
+      const error = new HubError({
+        errorCode: ErrorCode.NotImplemented,
+      });
+      const response = error.toResponse();
+      expect(response.errorCode).toEqual(ErrorCode.NotImplemented);
+      expect(response.developerMessage).toEqual(DeveloperMessage.NotImplemented);
+    })
   });
 });
