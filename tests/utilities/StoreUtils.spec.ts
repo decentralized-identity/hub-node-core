e7029a38d26fc703359513d0eb2247218b666dd4
Logan Girvin <logirvin@microsoft.com>
2018-12-03 17:35:45 -0800

    123 tests later and I am satisfied with the coverage.

--- tests/utilities/StoreUtils.spec.ts
+++ tests/utilities/StoreUtils.spec.ts
@@ -5,6 +5,9 @@ import { Context } from '../models/BaseRequest.spec';
 import * as store from '../../lib/interfaces/Store';
 import StoreUtils from '../../lib/utilities/StoreUtils';
 import { Operation } from '../../lib/models/Commit';
+import ObjectContainer from '../../lib/interfaces/ObjectContainer';
+import HubError, { ErrorCode } from '../../lib/models/HubError';
+import PermissionGrant from '../../lib/models/PermissionGrant';
 
 function getHex(): string {
   return Math.round(Math.random() * Number.MAX_SAFE_INTEGER).toString(16);
@@ -16,29 +19,35 @@ describe('StoreUtils', () => {
   const sender = `${owner}-not`;
   const hub = `did:example:${getHex()}`;
 
+  let commit: TestCommit;
+  let request: WriteRequest;
+  beforeEach(() => {
+    commit = TestCommit.create({
+      interface: getHex(),
+      context: getHex(),
+      type: getHex(),
+      object_id: getHex(),
+      sub: owner,
+      kid: `${sender}#key-1`,
+      operation: Operation.Update,
+    });
+    request = new WriteRequest({
+      iss: sender,
+      aud: hub,
+      sub: owner,
+      '@context': Context,
+      '@type': 'WriteRequest',
+      commit: {
+        protected: commit.getProtectedString(),
+        payload: commit.getPayloadString(),
+        signature: 'foobar',
+      },
+    });
+  });
+
   describe('objectExists', () => {
+
     it('should query store with the correct filters', async () => {
-      const commit = TestCommit.create({
-        interface: getHex(),
-        context: getHex(),
-        type: getHex(),
-        object_id: getHex(),
-        sub: owner,
-        kid: `${sender}#key-1`,
-        operation: Operation.Update,
-      });
-      const request = new WriteRequest({
-        iss: sender,
-        aud: hub,
-        sub: owner,
-        '@context': Context,
-        '@type': 'WriteRequest',
-        commit: {
-          protected: commit.getProtectedString(),
-          payload: commit.getPayloadString(),
-          signature: 'foobar',
-        },
-      });
       const spy = spyOn(store, "queryObjects").and.callFake((queryRequest: store.ObjectQueryRequest) => {
         expect(queryRequest.owner).toEqual(owner);
         expect(queryRequest.filters).toBeDefined();
@@ -66,5 +75,125 @@ describe('StoreUtils', () => {
       expect(spy).toHaveBeenCalled();
       expect(shouldBeFalse).toBeFalsy();
     });
+
+    it('should throw if multiple objects are returned for just one object_id', async () => {
+      const spy = spyOn(store, "queryObjects").and.callFake((_: store.ObjectQueryRequest) => {
+        const objects: ObjectContainer[] = [];
+        const count = Math.round(Math.random() * 10) + 2;
+        for (let i = 0; i < count; i++) {
+          objects.push({
+            interface: 'test',
+            context: 'context',
+            type: 'type',
+            id: getHex(),
+            created_by: owner,
+            created_at: new Date(Date.now()).toISOString(),
+            sub: owner,
+            commit_strategy: 'basic'
+          });
+        }
+        return {
+          results: objects,
+          pagination: {
+            skip_token: null,
+          },
+        };
+      });
+      try {
+        await StoreUtils.objectExists(request, store, []);
+        fail('expected a panic');
+      } catch (err) {
+        if (!(err instanceof HubError)) {
+          fail(err.message);
+        }
+        expect(err.errorCode).toEqual(ErrorCode.ServerError);
+      }
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should deny permission if no grant permits the object', async() => {
+      const spy = spyOn(store, "queryObjects").and.returnValue({
+        results: [
+          {
+            interface: 'test',
+            context: 'context',
+            type: 'type',
+            id: getHex(),
+            created_by: owner,
+            created_at: new Date(Date.now()).toISOString(),
+            sub: owner,
+            commit_strategy: 'basic'
+          }
+        ],
+        pagination: {
+          skip_token: null,
+        },
+      });
+      const grant: PermissionGrant = {
+        owner,
+        grantee: sender,
+        allow: 'CRUD-',
+        context: 'context',
+        type: 'type',
+        created_by: `did:example:${getHex()}`
+      };
+      try {
+        await StoreUtils.objectExists(request, store, [grant]);
+        fail('expected to throw');
+      } catch (err) {
+        if (!(err instanceof HubError)) {
+          fail(err.message);
+        }
+        expect(err.errorCode).toEqual(ErrorCode.PermissionsRequired);
+      }
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should return true if it exists and is granted', async () => {
+      const spy = spyOn(store, "queryObjects").and.returnValue({
+        results: [
+          {
+            interface: 'test',
+            context: 'context',
+            type: 'type',
+            id: getHex(),
+            created_by: owner,
+            created_at: new Date(Date.now()).toISOString(),
+            sub: owner,
+            commit_strategy: 'basic'
+          }
+        ],
+        pagination: {
+          skip_token: null,
+        },
+      });
+      const grant: PermissionGrant = {
+        owner,
+        grantee: sender,
+        allow: 'CRUD-',
+        context: 'context',
+        type: 'type',
+        created_by: owner
+      };
+      const shouldBeTrue = await StoreUtils.objectExists(request, store, [grant]);
+      expect(shouldBeTrue).toBeTruthy();
+      expect(spy).toHaveBeenCalled();
+    });
   });
+
+  describe('writeCommit', () => {
+    it('should format the request into a commitRequest', async () => {
+      const revision = getHex();
+      const spy = spyOn(store, 'commit').and.callFake((request: store.CommitRequest) => {
+        expect(request.owner).toEqual(owner);
+        expect(request.commit).toEqual(request.commit);
+        return {
+          knownRevisions: [revision],
+        };
+      });
+      const results = await StoreUtils.writeCommit(request, store);
+      expect(spy).toHaveBeenCalled();
+      expect(results.revisions).toEqual([revision]);
+    });
+  })
 })
